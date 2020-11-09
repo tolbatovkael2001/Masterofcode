df30aa5625604c9551e2b9c64ea58cf7e33f972b
Sasha Tolbatov <tolbatovsasha@ukr.net>
2020-11-09 15:48:31 +0200

    remove homework1

--- homework-01/homework_1/tasks/index.js
+++
@@ -1,9 +0,0 @@
-const sortProducts = require('./task1');
-const findMaxValue = require('./task2');
-const modifyProducts = require('./task3');
-
-module.exports = {
-   sortProducts,
-   findMaxValue,
-   modifyProducts,
-};
--- homework-01/homework_1/tasks/task1.js
+++
@@ -1,5 +0,0 @@
-function sortProducts(products, key, value) {
-   return products.filter((product) => product[key] === value);
-}
-
-module.exports = sortProducts;
